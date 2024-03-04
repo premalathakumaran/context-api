23a14f5e93659402b6318f00fc12cd8da603f3e1
premalathakumaran <premalathakumaran18@gmail.com>
2024-03-04 13:38:46 +0530

    context commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+
+
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer class="py-3 fixed-bottom  footer my-1">
+        <p class="text-center text-body-secondary">
+          ©Create by <span className="Name">premalatha</span>
+        </p>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+
+
+import React, { useContext } from "react";
+import SingleProduct from "./SingleProduct";
+import { CartContext } from "../ContextAPI/CartContext";
+
+function Home() {
+  const { products } = useContext(CartContext);
+  return (
+    <div className="productContainer">
+      {products.map((product) => (
+        <SingleProduct key={product.id} product={product} />
+      ))}
+    </div>
+  );
+}
+
+export default Home;
